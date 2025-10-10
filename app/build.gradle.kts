44552b5f0f0059e8c50e22909165263a546c45f4
wailanbrea <45805381+wailanbrea@users.noreply.github.com>
2025-10-09 21:41:33 -0400

    Configuracion de Firebase - Plugin de Google Services activado y google-services.json agregado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp") version "2.0.21-1.0.27"
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
